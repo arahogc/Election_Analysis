3369b473d23959870950b720f05919982d589065
arahogc <74072742+arahogc@users.noreply.github.com>
2020-12-02 17:44:46 -0800

    Updated Pypoll.py code to include county votes, created a copy of all code and named it PyPoll_Challenge.py, and updated the election_anlysis.txt file based on the election results.

--- Pypoll.py
+++ Pypoll.py
@@ -1,49 +1,42 @@
-#Data needed to retrieve 
-#1. The total number of votes cast
-#2. A complete list of candidates who received votes 
-#3. The precentage of votes each candidate won 
-#4. The total number of votes each candidate won 
-#5. The winner of the election based on popular vote. 
 
-#Add dependencies 
 import csv 
 import os
 
-#Assign a variabe to load a file from a path 
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-#Initialize a total vote counter 
 total_votes = 0 
-#Candidate options and candidate votes 
+
 candidate_options = []
-#Declare the empty dictionary 
 candidate_votes = {}
-#Winning candidate and winning count tracker 
 winning_candidate = " "
 winning_count = 0 
 winning_percentage = 0 
-#Open the election results and read the file
+
+county_options = []
+county_votes = {}
+largest_county_turnout = " "
+largest_vote = 0
+
 with open(file_to_load) as election_data: 
     file_reader = csv.reader(election_data)
-    
-    #read the header row 
     headers = next(file_reader)
-    #Print each row in the CSV File 
     for row in file_reader: 
-        #2. Add to the total vote count. 
         total_votes +=1 
-        #Print the candidate name from each row 
         candidate_name = row[2]
-        #If the candidate does not match any existing candidate
+
         if candidate_name not in candidate_options: 
-            #Add it to the list of candidates 
             candidate_options.append(candidate_name)
-            #Begin tracking that candidates's vote count 
             candidate_votes[candidate_name] =0 
-         #Add a vote for each candidate 
         candidate_votes[candidate_name] += 1
-        #Save the results to text file 
+
+        county_name = row[1]
+
+        if county_name not in county_options: 
+            county_options.append(county_name)
+            county_votes[county_name] = 0 
+        county_votes[county_name] += 1
+
 with open(file_to_save, "w") as txt_file: 
     election_results = ( 
         f"\nElection Results\n"
@@ -53,11 +46,32 @@ with open(file_to_save, "w") as txt_file:
     print(election_results, end= " ")
     txt_file.write(election_results)
 
+    for county_name in county_votes: 
+        c_votes = county_votes[county_name]
+        county_percentage = float(c_votes) / float(total_votes) * 100 
+        county_results = (
+            f"\n County Votes: \n"
+            f"{county_name}: {county_percentage:.1f}$ ({c_votes:,})\n")
+        print(county_results)
+        txt_file.write(county_results)
+        
+        if (c_votes > largest_vote): 
+            largest_vote = c_votes 
+            largest_county_turnout = county_name 
+            county_summary =( 
+                f"\n------------------\n"
+                f"Largest County Turnout: {largest_county_turnout}\n"
+                f"------------------\n")
+    print(county_summary)
+    txt_file.write(county_summary)
+
     for candidate_name in candidate_votes: 
         votes = candidate_votes[candidate_name]
         voter_percentage = float(votes) / float(total_votes) * 100 
         candidate_results = (
             f"{candidate_name}: {voter_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
 
         if (votes > winning_count) and (voter_percentage > winning_percentage): 
             winning_count = votes
@@ -66,7 +80,7 @@ with open(file_to_save, "w") as txt_file:
             winning_candidate_summary =( 
                 f"----------------------\n"
                 f"Winner: {winning_candidate}\n"
-                f"Winniing Vote Counte: {winning_count:,}\n"
+                f"Winniing Vote Count: {winning_count:,}\n"
                 f"Winning Percentage: {winning_percentage:.1f}%\n" 
                 f"----------------------\n")
             
